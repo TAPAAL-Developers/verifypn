dd93e51b75b334ec13b8d3cb54ebc06849b743b6
Mads Johannsen <Mads Johannsen>
2015-04-14 00:58:45 +0200

    Fixed bug in QueryStringParser and added deadlock support

--- PetriParse/QueryStringParser.h
+++ PetriParse/QueryStringParser.h
@@ -3,13 +3,14 @@
 
 #include "QueryXMLParser.h"
 #include "../PetriEngine/PetriNet.h"
+#include "PNMLParser.h"
 
 #include <string>
 #include <vector>
 
 class QueryStringParser {
     public:
-        QueryStringParser(QueryXMLParser *Parser, PetriEngine::PetriNet *PetriNet);
+        QueryStringParser(QueryXMLParser *Parser, PetriEngine::PetriNet *PetriNet, PNMLParser::InhibitorArcList inhibarcs);
         void generateStateLabel(int i);
         void generateStateLabels();
         string getStateLabel(int i);
@@ -18,10 +19,13 @@ class QueryStringParser {
         void replaceOperator(std::string& query, const std::string& from, const std::string& to);
         void replacePlaces(std::string& query);
         string  getPlaceIndexByName(const std::string placeName);
+        void findDeadlockConditions(std::string& query, size_t deadlockPos);
+        int inhibArc(unsigned int p, unsigned int t);
 
     private:
         QueryXMLParser *_Parser;
         PetriEngine::PetriNet *_PetriNet;
         std::vector<std::string> _stateLabel;
+        PNMLParser::InhibitorArcList _inhibArcs;
 };
 #endif	/* QUERYSTRINGPARSER_H */
