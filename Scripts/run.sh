6c6776a700f759f24b2608b05d493678ba7b5b8b
Søren Moss Nielsen <smni12@student.aau.dk>
2015-04-29 10:50:19 +0200

    Architecture cache size check added to script

--- Scripts/run.sh
+++ Scripts/run.sh
@@ -1,6 +1,14 @@
     export PREFIX=/home/mads/verifypnLTSmin/Scripts
     TOOL=BenchKit_head.sh
     INPUTSPATH=/home/mads/INPUTS
+	
+	CACHE_SIZE=$(cat /sys/devices/system/cpu/cpu0/cache/index0/coherency_line_size)
+
+    if [[ $CACHE_SIZE  !=  64 ]]
+    then
+        echo "!!!INCORRECT ARCHITECTURE - NOT 64-bit CACHE!!!"
+        exit
+    fi
 
 for D in $(find ${INPUTSPATH} -mindepth 1 -maxdepth 1 -type d) ; do
     echo;
