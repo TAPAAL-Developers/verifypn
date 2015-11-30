caec5ef98baec8c6b284115041259cead8fbeb12
LSJ lasjen12@student.aau.dk <LSJ lasjen12@student.aau.dk>
2015-11-30 14:38:33 +0100

    removed a few comments

--- CTL/edgepicker.cpp
+++ CTL/edgepicker.cpp
@@ -37,6 +37,7 @@ inline Edge* EdgePicker::DFS(){
 inline void EdgePicker::DFS(Edge* t_edge){
     W.push_front(t_edge);
 }
+
 //BFS pop
 inline Edge* EdgePicker::BFS(){
     Edge* e = W.front();
@@ -48,34 +49,13 @@ inline void EdgePicker::BFS(Edge* t_edge){
     W.push_back(t_edge);
 }
 
-
 //BestFS pop
 inline Edge* EdgePicker::BestFS(){
     Edge* e = W.front();
     W.pop_front(); 
-    return e; 
-   /*Edge* bestEdge = W.front();
-    int i = 0;
-    int lowestNR = bestEdge->targets.size();
-
-    for (auto e : W){
-        if(e->targets.size() < lowestNR){
-            if(lowestNR == 1){
-                W.erase(W.begin() + i);
-                return e;
-            } 
-        }
-        i++;
-        if(i > 10){
-            break;
-        }
-    }
-    Edge* e = W.front();
-    W.pop_front();
-    return e;*/
-
-
+    return e;
 }
+
 //BestFS push
 inline void EdgePicker::BestFS(Edge* t_edge){
     t_edge->rateEdge();
@@ -95,7 +75,5 @@ inline void EdgePicker::BestFS(Edge* t_edge){
 
     W.push_back(t_edge);
 }
-
-
 
 }
