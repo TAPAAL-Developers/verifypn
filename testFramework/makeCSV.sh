12fbca01f99449b4b77c8ecff38ccce8426f0e4e
Isabella Kaufmann ikaufm12@student.aau.dk <Isabella Kaufmann ikaufm12@student.aau.dk>
2015-12-08 18:15:44 +0100

    script

--- testFramework/makeCSV.sh
+++ testFramework/makeCSV.sh
@@ -1,26 +1,41 @@
+
 #!/bin/bash
 echo "Algorithm, Search strategy, Query, Modelname, #Results)" >> result.csv
 
 for R in *.log; do
-        awk '/Algorithm:/{print $NF}' $R >> meta.log
-	awk '/Search:/{print $NF}' $R >> meta.log
-	awk '/Query type:/{print $NF}' $R >> meta.log
-	awk '/FORMULA /{print $3}' $R | tr -d '\n' >> meta.log
-	echo "" >> meta.log
-	awk '/Modelname:/{print $NF}' $R >> meta.log
+    awk '/Algorithm:/{print $NF}' $R >> meta.log
+    sed -n '1p' meta.log >> tmp.log
+    rm meta.log
+    mv tmp.log meta.log
+
+    awk '/Search:/{print $NF}' $R >> meta.log
+        sed -n '1,2p' meta.log >> tmp.log
+    rm meta.log
+    mv tmp.log meta.log
+
+    awk '/Query type:/{print $NF}' $R >> meta.log
+    sed -n '1,3p' meta.log >> tmp.log
+    rm meta.log
+    mv tmp.log meta.log
+
+    awk '/Modelname:/{print $NF}' $R >> meta.log
+    sed -n '1,4p' meta.log >> tmp.log
+    rm meta.log
+    mv tmp.log meta.log
+
+     awk '/FORMULA/ {count++} END{print count}' $R >> meta.log
 
 
 
-
-	for P in meta.log; do
-	{
-		read engine
+    for P in meta.log; do
+    {
+        read engine
                 read search
-		read q
-		read formula
-		read name
-		echo "$engine, $search, $q, $name, $formula" >> result.csv
-	} < $P
-	done
-	rm meta.log
+        read q
+        read name
+        read formula
+        echo "$engine, $search, $q, $name, $formula" >> result.csv
+    } < $P
+    done
+    rm meta.log
 done
