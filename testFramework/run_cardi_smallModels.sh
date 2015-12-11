1dfdbe18f201f27e60ead32bdb73fa65ea3c6e26
Søren Moss Nielsen <smni12@student.aau.dk>
2015-12-11 14:47:58 +0100

    Update

--- testFramework/run_cardi_smallModels.sh
+++ testFramework/run_cardi_smallModels.sh
@@ -3,23 +3,23 @@ RUNPATH=`pwd`
 
 MODELFILE="./model.pnml"
 ENGINE="-ctl"
-TOOL="DGEngine.sh"
-ALGORITHM=("local" "global" "czero")
-STRATEGY=("BFS" "DFS" "BestFS" "CDFS" "FBFS" "BBFS" "BDFS")
+TOOL="../../DGEngine_singleQuery.sh"
+ALGORITHM=("czero")
+STRATEGY=("DFS")
 ANALYSE="analyse.sh"
 MEMORY="mem.sh"
 
 REL_PROGRAMPATH="../verifypn-linux64"
-INPUTSPATH="$RUNPATH/testModels/"
+INPUTSPATH="$RUNPATH/smallModels/"
 RESULTPATH="$RUNPATH/testResults/"
 ANALISEPATH="$RUNPATH/analysis"
 
-if [ -f $REL_PROGRAMPATH ] 
-	then
-	PROGRAMPATH=$(cd $(dirname $REL_PROGRAMPATH); pwd)/$(basename $REL_PROGRAMPATH)
+if [ -f $REL_PROGRAMPATH ]
+        then
+        PROGRAMPATH=$(cd $(dirname $REL_PROGRAMPATH); pwd)/$(basename $REL_PROGRAMPATH)
 else
-	echo "Cannot find file $PROGRAMPATH"
-	exit 1
+        echo "Cannot find file $PROGRAMPATH"
+        exit 1
 fi
 
 echo "####################################################################";
@@ -39,51 +39,51 @@ for D in $(find ${INPUTSPATH} -mindepth 1 -maxdepth 1 -type d) ; do
     cp $TOOL $D
     #cp $MEMORY $D
     cd $D ;
-    
-	export mname="`basename "$PWD"`"
 
-	#verify model exists
-	if [ -f $MODELFILE ] ; then
+        export mname="`basename "$PWD"`"
 
-		for QF in $(find -mindepth 1 -maxdepth 1 -type f -name 'CTLCardinality.xml') ; do
+        #verify model exists
+        if [ -f $MODELFILE ] ; then
 
-		 	qfile="$(basename $QF)"
-		 	echo "$qfile"
-			export qname="${qfile%.*}"
+                for QF in $(find -mindepth 1 -maxdepth 1 -type f -name 'CTL*.xml') ; do
 
-			for A in ${ALGORITHM[@]}; do
-				export aname="$A"
-				echo "$A"
-                                
+                        qfile="$(basename $QF)"
+                        echo "$qfile"
+                        export qname="${qfile%.*}"
 
-				for S in ${STRATEGY[@]}; do
-					export sname="$S"
-					echo "$S"
+                        for A in ${ALGORITHM[@]}; do
+                                export aname="$A"
+                                echo "$A"
 
-					if [[ "$S" == CDFS && "$A" != "czero" ]]; then
-						echo"incompatible algorithm and search strategy"
-					else
-						./$TOOL $PROGRAMPATH $MODELFILE $QF $ENGINE 1 & ./$TOOL $PROGRAMPATH $MODELFILE $QF $ENGINE 2
-					 	##{ ./$MEMORY; } > "$mname-$qname-$aname-$sname-mem.log" 
-					 	#cat "$mname-$qname-$aname-$sname.log" "$mname-$qname-$aname-$sname-mem.log" >> "$mname-$qname-$aname-$sname-all.log"
-					 	#rm "$mname-$qname-$aname-$sname-mem.log"
-					 	mv $mname-$qname-$aname-$sname.log ../../testResults/$mname-$qname-$aname-$sname.log         
-					fi
-					                    
-			 	done
-			done
-		 done
-	else
-		echo "Cannot find $MODELFILE in $MODELPATH"
-	fi
 
-	#remove scripts after usage
-	rm $TOOL
-	#rm $MEMORY
-	cd ../..
-	#for log in $(find -mindepth 1 -maxdepth 1 -type f -name '*.log'); do
-	#	mv "$log" "$RESULTPATH"
-	#done
+                                for S in ${STRATEGY[@]}; do
+                                        export sname="$S"
+                                        echo "$S"
+
+                                        if [[ "$S" == CDFS && "$A" != "czero" ]]; then
+                                                echo"incompatible algorithm and search strategy"
+                                        else
+                                        		./$TOOL $PROGRAMPATH $MODELFILE $QF $ENGINE 1 & ./$TOOL $PROGRAMPATH $MODELFILE $QF $ENGINE 2 & ./$TOOL $PROGRAMPATH $MODELFILE $QF $ENGINE 3 & ./$TOOL $PROGRAMPATH $MODELFILE $QF $ENGINE 4 & ./$TOOL $PROGRAMPATH $MODELFILE $QF $ENGINE 5 & ./$TOOL $PROGRAMPATH $MODELFILE $QF $ENGINE 6 & ./$TOOL $PROGRAMPATH $MODELFILE $QF $ENGINE 7 & ./$TOOL $PROGRAMPATH $MODELFILE $QF $ENGINE 8 & ./$TOOL $PROGRAMPATH $MODELFILE $QF $ENGINE 9 & ./$TOOL $PROGRAMPATH $MODELFILE $QF $ENGINE 10 & ./$TOOL $PROGRAMPATH $MODELFILE $QF $ENGINE 11 & ./$TOOL $PROGRAMPATH $MODELFILE $QF $ENGINE 12 & ./$TOOL $PROGRAMPATH $MODELFILE $QF $ENGINE 13 & ./$TOOL $PROGRAMPATH $MODELFILE $QF $ENGINE 14 & ./$TOOL $PROGRAMPATH $MODELFILE $QF $ENGINE 15 & ./$TOOL $PROGRAMPATH $MODELFILE $QF $ENGINE 16
+                                                ##{ ./$MEMORY; } > "$mname-$qname-$aname-$sname-mem.log" 
+                                                #cat "$mname-$qname-$aname-$sname.log" "$mname-$qname-$aname-$sname-mem.log" >> "$mname-$qname-$aname-$sname-all.log"
+                                                #rm "$mname-$qname-$aname-$sname-mem.log"
+                                                mv $mname-$qname-$aname-$sname.log ../../testResults/$mname-$qname-$aname-$sname.log
+                                        fi
+
+                                done
+                        done
+                 done
+        else
+                echo "Cannot find $MODELFILE in $MODELPATH"
+        fi
+
+        #remove scripts after usage
+#       rm $TOOL
+        #rm $MEMORY
+        cd ../..
+        #for log in $(find -mindepth 1 -maxdepth 1 -type f -name '*.log'); do
+        #       mv "$log" "$RESULTPATH"
+        #done
 
 done
 
