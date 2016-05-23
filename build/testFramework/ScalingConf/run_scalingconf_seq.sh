a55c318195657f9fd1ed226a04351f6aaf62181e
Tobias Jepsen tobiasj1991@gmail.com <Tobias Jepsen tobiasj1991@gmail.com>
2016-05-23 09:54:56 +0200

    Solitaire restart sequential

--- build/testFramework/ScalingConf/run_scalingconf_seq.sh
+++ build/testFramework/ScalingConf/run_scalingconf_seq.sh
@@ -87,20 +87,6 @@ do
     done
 done
 
-# Compute 7
-# Solitaire-PT-SqrNC5x5
-for model in Solitaire-PT-SqrNC5x5;
-do
-    for worker_count in 1;
-    do
-        for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16;
-        do
-            let timeout="$max_timeout"
-            sbatch /user/smni12/launchpad/master/build/testFramework/ScalingConf/compute7_seq.sh "$model" "$ALG" "$timeout" "$i" "$RUN_NO"
-        done
-    done
-done
-
 # Compute 8
 # BridgeAndVehicles-PT-V20P20N10
 for model in Solitaire-PT-SqrNC5x5;
@@ -128,3 +114,66 @@ do
         done
     done
 done
+
+
+	for model in Solitaire-PT-SqrNC5x5;
+	do
+	    for worker_count in "czero" "local";
+	    do
+		# Compute 7
+		for i in 1 2;
+		do
+		    let timeout="$max_timeout"
+		    sbatch /user/smni12/launchpad/master/build/testFramework/ScalingConf/compute7_seq.sh "$model" "$worker_count" "$timeout" "$i" "$RUN_NO"
+		done
+
+		# Compute 5
+		for i in 3 4;
+		do
+		    let timeout="$max_timeout"
+		    sbatch /user/smni12/launchpad/master/build/testFramework/ScalingConf/compute5_seq.sh "$model" "$worker_count" "$timeout" "$i" "$RUN_NO"
+		done
+
+		# Compute 9
+		for i in 5 6;
+		do
+		    let timeout="$max_timeout"
+		    sbatch /user/smni12/launchpad/master/build/testFramework/ScalingConf/compute9_seq.sh "$model" "$worker_count" "$timeout" "$i" "$RUN_NO"
+		done
+
+		# Compute 6
+		for i in 7 8;
+		do
+		    let timeout="$max_timeout"
+		    sbatch /user/smni12/launchpad/master/build/testFramework/ScalingConf/compute6_seq.sh "$model" "$worker_count" "$timeout" "$i" "$RUN_NO"
+		done
+
+		# Compute 4
+		for i in 9 10;
+		do
+		    let timeout="$max_timeout"
+		    sbatch /user/smni12/launchpad/master/build/testFramework/ScalingConf/compute4_seq.sh "$model" "$worker_count" "$timeout" "$i" "$RUN_NO"
+		done
+	
+		# Compute 8
+		for i in 11 12;
+		do
+		    let timeout="$max_timeout"
+		    sbatch /user/smni12/launchpad/master/build/testFramework/ScalingConf/compute8_seq.sh "$model" "$worker_count" "$timeout" "$i" "$RUN_NO"
+		done
+
+		# Compute 2
+		for i in 13 14;
+		do
+		    let timeout="$max_timeout"
+		    sbatch /user/smni12/launchpad/master/build/testFramework/ScalingConf/compute2_seq.sh "$model" "$worker_count" "$timeout" "$i" "$RUN_NO"
+		done
+
+		# Compute 1
+		for i in 15 16;
+		do
+		    let timeout="$max_timeout"
+		    sbatch /user/smni12/launchpad/master/build/testFramework/ScalingConf/compute1_seq.sh "$model" "$worker_count" "$timeout" "$i" "$RUN_NO"
+		done
+	    done
+	done
