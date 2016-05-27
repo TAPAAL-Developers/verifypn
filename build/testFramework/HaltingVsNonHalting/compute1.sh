78a4e089e2d4f0c092ae8682f6da9c2077e75d29
Tobias Jepsen tobiasj1991@gmail.com <Tobias Jepsen tobiasj1991@gmail.com>
2016-05-27 14:10:05 +0200

    Halting script

---
+++ build/testFramework/HaltingVsNonHalting/compute1.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+#SBATCH --no-requeue
+#SBATCH --mem 1000000
+#SBATCH --nodes=1
+#SBATCH --partition=production
+#SBATCH --ntasks=1
+#SBATCH --exclusive
+#SBATCH --mail-type=ALL # Type of email notification- BEGIN,END,FAIL,ALL
+#SBATCH --mail-user={d803f16@cs.aau.dk}
+#SBATCH --time=00:30:05
+#SBATCH --nodelist=compute1
+
+WORKERS=$1
+TIMEOUT=$2
+RUN_NO=$3
+BINARY=$4
+OUTPUTFILE="$BINARY"-CTLCardinality-"$I"-W"$WORKERS"-T"$TIMEOUT"-R"$RUN_NO".log
+
+mkdir ~/results
+mkdir ~/results/master
+mkdir ~/results/master/HaltingVsNonHalting
+mkdir ~/results/master/HaltingVsNonHalting/W"$WORKERS"-R"$RUN_NO"
+
+ulimit -S -v 1024000000
+ulimit -l 1024000000
+
+export MAXMEM_KB=16000000
+{ timeout "$TIMEOUT" mpirun -np "$WORKERS" /user/smni12/launchpad/master/"$BINARY" /user/smni12/launchpad/modelDatabase/allModels/HouseConstruction-PT-010/model.pnml /user/smni12/launchpad/modelDatabase/allModels/HouseConstruction-PT-005/CTLCardinality.xml -ctl dist -s DFS -x 12; } >> ~/results/master/HaltingVsNonHalting/W"$WORKERS"-R"$RUN_NO"/$OUTPUTFILE 2>&1
+
+ulimit -S -v unlimited
+
+echo Successful result move
+echo $SLURM_NODELIST
+echo $SLURM_NPROCS
+pwd
+hostname
+rm -- "$0"
---
+++ build/testFramework/HaltingVsNonHalting/run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+RUN_NO="$1"
+max_timeout=60
+BINARY=verifypn-linux64-nonhalting
+
+# Compute 1
+for binary in verifypn-linux64-nonhalting verifypn-linux64;
+do
+    for worker_count in 1 2 4 8 16 32 48 64;
+    do
+            let timeout="$max_timeout"
+            sbatch /user/smni12/launchpad/master/build/testFramework/HaltingVsNonHalting/compute1.sh "$worker_count" "$timeout" "$BINARY"-"$RUN_NO" "$BINARY"
+        done
+    done
+done
