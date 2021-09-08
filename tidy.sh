075d102b31a562fc3767a490bbaf4e5815d97f75
Peter G. Jensen <root@petergjoel.dk>
2021-09-08 21:32:02 +0200

    script for doing all the tidy

---
+++ tidy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+F=$(find $SCRIPT_DIR/src $SCRIPT_DIR/include -type f -name '*.cpp' -o -name '*.h' -o -name '*.hpp' -o -name '*.c' | grep -v ".lexer." | grep -v ".parser.")
+clang-tidy $F -fix -extra-arg-before=-xc++-header -extra-arg=-std=c++17 -extra-arg=-I./$SCRIPT_DIR/include -extra-arg=-I$SCRIPT_DIR/build/external/include -p $SCRIPT_DIR/build/compile_commands
