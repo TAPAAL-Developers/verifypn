61a97d460d65b20f169789df9df4ea1d242f26b6
Nikolaj Jensen Ulrik <njul@cs.aau.dk>
2021-12-10 11:15:13 +0100

    both optimisations

--- src/CTL/Algorithm/CertainZeroFPA.cpp
+++ src/CTL/Algorithm/CertainZeroFPA.cpp
@@ -48,11 +48,11 @@ bool Algorithm::CertainZeroFPA::search(DependencyGraph::BasicDependencyGraph &t_
 void Algorithm::CertainZeroFPA::checkEdge(Edge* e, bool only_assign)
 {
     if(e->handled) return;
-    /*if(e->source->isDone())
+    if(e->source->isDone())
     {
         if(e->refcnt == 0) graph->release(e);
         return;
-    }*/
+    }
 
     bool allDone = e->source != vertex;
     for (auto *pre : e->source->dependency_set) {
